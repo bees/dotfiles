d519a67f64e726a607b9426e5061bc4ece1995c0
bees <arian.daneshvar@gmail.com>
2022-12-22 15:57:30 -0800

    use vim.colorscheme
    treesitter was exploding with the vim.cmd syntax

--- nvim/.config/nvim/lua/bees/plugins.lua
+++ nvim/.config/nvim/lua/bees/plugins.lua
@@ -1,19 +1,20 @@
--- Only required if you have packer configured as `opt`
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use {
+  use({
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { { 'nvim-lua/plenary.nvim' } }
-  }
+  })
+
   use({
     'rose-pine/neovim',
     as = 'rose-pine',
     config = function()
-      vim.cmd('colorscheme rose-pine')
+      vim.colorscheme('rose-pine')
     end
   })
+
   use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
   use 'nvim-treesitter/playground'
   use 'theprimeagen/harpoon'
