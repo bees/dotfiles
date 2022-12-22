d1fa1d51e22bbe54fe1236e8e2adaa884de70be0
bees <arian.daneshvar@gmail.com>
2022-12-22 15:34:46 -0800

    set background initially to avoid flicker

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,8 @@
+if (io.popen("uname -s"):read '*a'):match 'Darwin' then
+    if (io.popen('defaults read -g AppleInterfaceStyle'):read '*a'):match 'Dark' then
+        vim.api.nvim_set_option('background', 'dark')
+      else
+        vim.api.nvim_set_option('background', 'light')
+    end
+end
 require('bees')
