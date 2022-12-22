c562acf59ed7e092f52195451cd0e55599c4c9a9
bees <arian.daneshvar@gmail.com>
2022-12-22 15:57:18 -0800

    setup telescope file picker extension

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,17 @@
+require("telescope").setup {
+  extensions = {
+    file_browser = {
+      hijack_netrw = true,
+    }
+  }
+}
+local telescope = require("telescope")
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+telescope.load_extension "file_browser"
+vim.keymap.set('n', '<leader>e', telescope.extensions.file_browser._picker, {})
